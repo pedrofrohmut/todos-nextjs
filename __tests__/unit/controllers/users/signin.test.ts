827365a983f86b83d4d4f5d5d1617abf9121363f
pedrofrohmut <pedrofrohmut@gmail.com>
2021-05-17 11:05:28 -0300

    test sign in controller

--- __tests__/unit/controllers/users/signin.test.ts
+++ __tests__/unit/controllers/users/signin.test.ts
@@ -7,6 +7,7 @@ import DeleteUserByEmailService from "../../../../server/services/users/implemen
 import GeneratePasswordHashService from "../../../../server/services/users/implementations/generate-password-hash.service"
 
 import { SERVER_URL } from "../../../constants"
+import FindUserByEmailService from "../../../../server/services/users/implementations/find-by-email.service"
 
 const URL = SERVER_URL + "/api/users/signin"
 
@@ -22,18 +23,44 @@ describe("[Controller] SignIn User", () => {
     ConnectionFactory.closeConnection(conn)
   })
 
+  test("Can be found by route", async () => {
+    try {
+      // Test
+      await axios.post(URL, {})
+    } catch (err) {
+      // Evaluation
+      expect(err.response.status).not.toBe(404)
+    }
+  })
+
+  test("Return bad request with invalid body", async () => {
+    try {
+      // Test
+      await axios.post(URL, {})
+    } catch (err) {
+      // Evaluation
+      expect(err.response.status).toBe(400)
+    }
+  })
+
   test("Return status ok with valid credentials", async () => {
     const userDataAccess = new UserDataAccess(conn)
     const generatePasswordHashService = new GeneratePasswordHashService()
     const createUserService = new CreateUserService(generatePasswordHashService, userDataAccess)
+    const findUserByEmailService = new FindUserByEmailService(userDataAccess)
     // Setup
+    const name = "John Doe 171"
     const email = "john_doe171@mail.com"
     const password = "password171"
-    await createUserService.execute({ name: "John Doe 171", email, password })
+    await createUserService.execute({ name, email, password })
+    const createdUser = await findUserByEmailService.execute(email)
     // Test
     const response = await axios.post(URL, { email, password })
     // Evaluation
     expect(response.status).toBe(200)
+    expect(response.data.user.id).toBe(createdUser.id)
+    expect(response.data.user.name).toBe(name)
+    expect(response.data.user.email).toBe(email)
     expect(response.data.token).toBeDefined()
     // Clean Up
     await new DeleteUserByEmailService(userDataAccess).execute(email)
